ef1b12c8f22138c3812e9a91e562f716567451d7
myshazaman13 <146783271+myshazaman13@users.noreply.github.com>
2024-09-06 00:33:18 +0100

    Add files via upload

---
+++ rmenu.js
@@ -0,0 +1,9 @@
+// responsive hamburder menu
+    function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+    }
+    function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+    }
